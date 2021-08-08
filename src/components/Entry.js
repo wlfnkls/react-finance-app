ce9169185b0df3c77e795f0250faed2210e47381
Wolf Niklas <niklas.schellhoeh@gmail.com>
2021-08-08 09:59:17 +0200

    Edit font color

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,7 +1,10 @@
 const Entry = ({ text, value }) => (
-  <div className="bg-white rounded mb-2 p-3 flex justify-between">
-    <h2 className="font-semibold">{text}</h2>
-    <h3 className={value < 0 ? 'text-red-800' : ''}>{`${value} €`}</h3>
+  <div className="bg-white rounded mb-2 p-3">
+    <div className="flex justify-between">
+      <h2 className="font-semibold">{text}</h2>
+      <span className={`font-semibold ${value < 0 ? 'text-red-500' : 'text-green-500'}`}>{`${value} €`}</span>
+    </div>
+    <h3 className="text-sm text-gray-500">19.02.2020</h3>
   </div>
 );
 
